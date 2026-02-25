092f68ed2b9f569c50b81408f66c8adac3c8ef33
NriotHrreion <nriot233@outlook.com>
2026-02-25 15:59:09 +0800

    fix: Cannot run on mac os

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import type { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
